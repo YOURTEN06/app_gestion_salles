2e720080cb791b799cd7a484833e768f3d653209
yourten bouchetit <yourten.bouchetit@monboreal.ca>
2026-04-20 04:45:20 -0400

    Ajout des validations métier dans ajouter_salle

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-from Data.dao_salle import DataSalle
+from services.services_salle import ServiceSalle
 from models.salle import Salle
 
-dao = DataSalle()
+service = ServiceSalle()
 
-# Ajout
-s1 = Salle("E404", "Salle multimédia", "Classe", 28)
-dao.insert_salle(s1)
-print("Ajout réussi")
+salle = Salle("F101", "Salle de test", "Classe", 20)
+succes, message = service.ajouter_salle(salle)
 
-# Recherche
-salle = dao.get_salle("E404")
-if salle:
-    print("Recherche :", salle.afficher_infos())
-
-# Modification
-s1_modifiee = Salle("E404", "Grande salle multimédia", "Classe", 35)
-dao.update_salle(s1_modifiee)
-print("Modification réussie")
-
-# Affichage de toutes les salles
-print("Liste des salles :")
-for s in dao.get_salles():
-    print(s.afficher_infos())
-
-# Suppression
-dao.delete_salle("E404")
-print("Suppression réussie")
+print(message)
