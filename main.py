1c95ea47607569b6e560bb3965ef3982f3a92c6a
yourten bouchetit <yourten.bouchetit@monboreal.ca>
2026-04-20 04:52:10 -0400

    Ajout du cadre Informations Salle dans l'interface

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-from services.services_salle import ServiceSalle
-from models.salle import Salle
+from views.view_salle import ViewSalle
 
-service = ServiceSalle()
-
-print("=== AJOUT ===")
-s1 = Salle("G201", "Salle de conférence", "Bureau", 15)
-succes, message = service.ajouter_salle(s1)
-print(message)
-
-print("\n=== RECHERCHE ===")
-salle = service.rechercher_salle("G201")
-if salle:
-    print(salle.afficher_infos())
-
-print("\n=== MODIFICATION ===")
-s1_modifiee = Salle("G201", "Grande salle de conférence", "Bureau", 22)
-succes, message = service.modifier_salle(s1_modifiee)
-print(message)
-
-print("\n=== LISTE ===")
-for s in service.recuperer_salles():
-    print(s.afficher_infos())
-
-print("\n=== SUPPRESSION ===")
-service.supprimer_salle("G201")
-print("Suppression réussie")
+app = ViewSalle()
+app.mainloop()
--- views/view_salle.py
+++ views/view_salle.py
@@ -10,3 +10,31 @@ class ViewSalle(ctk.CTk):
         self.geometry("700x500")
 
         self.service_salle = ServiceSalle()
+
+        # Cadre Informations Salle
+        self.cadreInfo = ctk.CTkFrame(self, corner_radius=10)
+        self.cadreInfo.pack(pady=20, padx=20, fill="x")
+
+        # Code
+        self.label_code = ctk.CTkLabel(self.cadreInfo, text="Code salle")
+        self.label_code.grid(row=0, column=0, padx=10, pady=10)
+        self.entry_code = ctk.CTkEntry(self.cadreInfo)
+        self.entry_code.grid(row=0, column=1, padx=10, pady=10)
+
+        # Description
+        self.label_description = ctk.CTkLabel(self.cadreInfo, text="Description")
+        self.label_description.grid(row=1, column=0, padx=10, pady=10)
+        self.entry_description = ctk.CTkEntry(self.cadreInfo)
+        self.entry_description.grid(row=1, column=1, padx=10, pady=10)
+
+        # Catégorie
+        self.label_categorie = ctk.CTkLabel(self.cadreInfo, text="Catégorie")
+        self.label_categorie.grid(row=2, column=0, padx=10, pady=10)
+        self.entry_categorie = ctk.CTkEntry(self.cadreInfo)
+        self.entry_categorie.grid(row=2, column=1, padx=10, pady=10)
+
+        # Capacité
+        self.label_capacite = ctk.CTkLabel(self.cadreInfo, text="Capacité")
+        self.label_capacite.grid(row=3, column=0, padx=10, pady=10)
+        self.entry_capacite = ctk.CTkEntry(self.cadreInfo)
+        self.entry_capacite.grid(row=3, column=1, padx=10, pady=10)
