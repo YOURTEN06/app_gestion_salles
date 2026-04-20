2259c479039462b87154a7b6c11a000b8d0947c5
yourten bouchetit <yourten.bouchetit@monboreal.ca>
2026-04-20 04:47:24 -0400

    Tests de la couche Service dans main.py

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from services.services_salle import ServiceSalle
+from models.salle import Salle
 
 service = ServiceSalle()
 
-print("Recherche d'une salle :")
-salle = service.rechercher_salle("F101")
+print("=== AJOUT ===")
+s1 = Salle("G201", "Salle de conférence", "Bureau", 15)
+succes, message = service.ajouter_salle(s1)
+print(message)
+
+print("\n=== RECHERCHE ===")
+salle = service.rechercher_salle("G201")
 if salle:
     print(salle.afficher_infos())
-else:
-    print("Salle non trouvée")
 
-print("\nListe des salles :")
+print("\n=== MODIFICATION ===")
+s1_modifiee = Salle("G201", "Grande salle de conférence", "Bureau", 22)
+succes, message = service.modifier_salle(s1_modifiee)
+print(message)
+
+print("\n=== LISTE ===")
 for s in service.recuperer_salles():
     print(s.afficher_infos())
 
-print("\nSuppression de la salle F101")
-service.supprimer_salle("F101")
+print("\n=== SUPPRESSION ===")
+service.supprimer_salle("G201")
 print("Suppression réussie")
