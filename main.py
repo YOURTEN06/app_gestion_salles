0154a9cc4c9f61216db51f6f9ba009b2475beb84
yourten bouchetit <yourten.bouchetit@monboreal.ca>
2026-04-20 04:43:17 -0400

    Tests CRUD de la couche DAO dans main.py

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from Data.dao_salle import DataSalle
+from models.salle import Salle
 
 dao = DataSalle()
-liste = dao.get_salles()
 
-for salle in liste:
-    print(salle.afficher_infos())
+# Ajout
+s1 = Salle("E404", "Salle multimédia", "Classe", 28)
+dao.insert_salle(s1)
+print("Ajout réussi")
+
+# Recherche
+salle = dao.get_salle("E404")
+if salle:
+    print("Recherche :", salle.afficher_infos())
+
+# Modification
+s1_modifiee = Salle("E404", "Grande salle multimédia", "Classe", 35)
+dao.update_salle(s1_modifiee)
+print("Modification réussie")
+
+# Affichage de toutes les salles
+print("Liste des salles :")
+for s in dao.get_salles():
+    print(s.afficher_infos())
+
+# Suppression
+dao.delete_salle("E404")
+print("Suppression réussie")
