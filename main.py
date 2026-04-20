17cc715af7020418a6641202ac825f1a42b996e7
yourten bouchetit <yourten.bouchetit@monboreal.ca>
2026-04-20 04:46:16 -0400

    Ajout des validations métier dans modifier_salle

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from models.salle import Salle
 
 service = ServiceSalle()
 
-salle = Salle("F101", "Salle de test", "Classe", 20)
-succes, message = service.ajouter_salle(salle)
+salle = Salle("F101", "Salle test modifiée", "Classe", 35)
+succes, message = service.modifier_salle(salle)
 
 print(message)
