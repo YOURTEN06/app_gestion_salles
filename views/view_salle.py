1343011039dbd68193b2041855bbd240ae1cc406
yourten bouchetit <yourten.bouchetit@monboreal.ca>
2026-04-20 05:11:30 -0400

    Version finale prête pour la soumission

--- views/view_salle.py
+++ views/view_salle.py
@@ -12,35 +12,35 @@ class ViewSalle(ctk.CTk):
 
         self.service_salle = ServiceSalle()
 
-        # Cadre Informations Salle
+
         self.cadreInfo = ctk.CTkFrame(self, corner_radius=10)
         self.cadreInfo.pack(pady=20, padx=20, fill="x")
 
-        # Code
+
         self.label_code = ctk.CTkLabel(self.cadreInfo, text="Code salle")
         self.label_code.grid(row=0, column=0, padx=10, pady=10)
         self.entry_code = ctk.CTkEntry(self.cadreInfo)
         self.entry_code.grid(row=0, column=1, padx=10, pady=10)
 
-        # Description
+
         self.label_description = ctk.CTkLabel(self.cadreInfo, text="Description")
         self.label_description.grid(row=1, column=0, padx=10, pady=10)
         self.entry_description = ctk.CTkEntry(self.cadreInfo)
         self.entry_description.grid(row=1, column=1, padx=10, pady=10)
 
-        # Catégorie
+
         self.label_categorie = ctk.CTkLabel(self.cadreInfo, text="Catégorie")
         self.label_categorie.grid(row=2, column=0, padx=10, pady=10)
         self.entry_categorie = ctk.CTkEntry(self.cadreInfo)
         self.entry_categorie.grid(row=2, column=1, padx=10, pady=10)
 
-        # Capacité
+
         self.label_capacite = ctk.CTkLabel(self.cadreInfo, text="Capacité")
         self.label_capacite.grid(row=3, column=0, padx=10, pady=10)
         self.entry_capacite = ctk.CTkEntry(self.cadreInfo)
         self.entry_capacite.grid(row=3, column=1, padx=10, pady=10)
 
-        # Cadre Actions
+
         self.cadreAction = ctk.CTkFrame(self, corner_radius=10)
         self.cadreAction.pack(pady=10, padx=20, fill="x")
 
@@ -56,7 +56,7 @@ class ViewSalle(ctk.CTk):
         self.btn_rechercher = ctk.CTkButton(self.cadreAction, text="Rechercher", command=self.rechercher_salle)
         self.btn_rechercher.grid(row=0, column=3, padx=10, pady=10)
 
-        # Cadre Liste des salles
+
         self.cadreList = ctk.CTkFrame(self, corner_radius=10)
         self.cadreList.pack(pady=10, padx=20, fill="both", expand=True)
 
@@ -66,13 +66,13 @@ class ViewSalle(ctk.CTk):
             show="headings"
         )
 
-        # Titres colonnes
+
         self.treeList.heading("code", text="CODE")
         self.treeList.heading("description", text="Description")
         self.treeList.heading("categorie", text="Catégorie")
         self.treeList.heading("capacite", text="Capacité")
 
-        # Largeur
+
         self.treeList.column("code", width=80)
         self.treeList.column("description", width=200)
         self.treeList.column("categorie", width=120)
